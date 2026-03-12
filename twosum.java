bb38cf92306b07d4da1ad67e014ca7080755fa19
DHANUSH824806 <dhanush824806@gmail.com>
2026-03-12 20:21:37 +0530

    this the solution for the problem number 1

---
+++ twosum.java
@@ -0,0 +1,33 @@
+// LeetCode Problem:1
+// It show the index number of the target 
+import java.util.*;
+class twosum {
+    public static int[] twoSum(int[] nums, int target) {
+      int i=0;
+      int j=nums.length-1;
+      while(i<j){
+        
+        ;
+        if(nums[i]+nums[j]==target){
+            return new int[]{i,j};
+        }
+        else if(nums[i]+nums[j]>target){
+            j--;
+        }
+        else{
+            i++;
+        }
+      }
+
+      return new int[]{};
+
+        
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target=13;
+        int[] res=twoSum(nums,target);
+        System.out.println(Arrays.toString(res));
+        
+    }
+}
